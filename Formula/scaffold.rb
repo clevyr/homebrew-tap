83b558499b63bd262be75808d406ce00a60a8f83
goreleaserbot <goreleaser@carlosbecker.com>
2020-10-02 13:30:52 -0500

    Brew formula update for scaffold version v0.3.9

--- Formula/scaffold.rb
+++ Formula/scaffold.rb
@@ -2,16 +2,16 @@
 class Scaffold < Formula
   desc ""
   homepage ""
-  version "0.3.8"
+  version "0.3.9"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/clevyr/scaffold/releases/download/v0.3.8/scaffold_0.3.8_Darwin_x86_64.tar.gz"
-    sha256 "5858df72eaf985d8374b55a5ad59f611911f83c22ddb22fee0ce6b61e21e8528"
+    url "https://github.com/clevyr/scaffold/releases/download/v0.3.9/scaffold_0.3.9_Darwin_x86_64.tar.gz"
+    sha256 "f273e7e3d8afbc84417c48805565bde80b74cec430c64a13a254cac9b4994d81"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/clevyr/scaffold/releases/download/v0.3.8/scaffold_0.3.8_Linux_x86_64.tar.gz"
-      sha256 "2ceb76ba317c29504ffe1e93443e9adbb04ea88b82ecbe9806904877863a9449"
+      url "https://github.com/clevyr/scaffold/releases/download/v0.3.9/scaffold_0.3.9_Linux_x86_64.tar.gz"
+      sha256 "68b8143f4694cf6fa96ea055fba173f384fe18895a66ca8317f4afca0bc8c437"
     end
   end
 
